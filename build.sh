5bc43b14095ca800ba8366f958dcd4008f55fa5f
Marco Ambrosio <62962632+marco-ambrosio@users.noreply.github.com>
2023-10-12 10:42:20 +0100

    added MAKEFLAGS to limit CPU usage

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
+export MAKEFLAGS="-j 4" # 4 is the number of cores the build system is allowed to use
+
 # Set the default build type
 BUILD_TYPE=RelWithDebInfo
 colcon build \
-        --merge-install \
         --symlink-install \
+        --executor sequential \ 
         --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
         -Wall -Wextra -Wpedantic

53d3bdf32148ddd3f8f4e5009703c6df97f51f18
Marco Ambrosio <62962632+marco-ambrosio@users.noreply.github.com>
2023-11-09 21:30:20 +0000

    limit parallel build and cpu usage by default

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-export MAKEFLAGS="-j 4" # 4 is the number of cores the build system is allowed to use
+export MAKEFLAGS="-j 6" # 6 is the number of cores the build system is allowed to use
 
 # Set the default build type
 BUILD_TYPE=RelWithDebInfo
 colcon build \
-        --symlink-install \
-        --executor sequential \ 
-        --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
-        -Wall -Wextra -Wpedantic
+--symlink-install \
+--parallel-workers 3 \ 
+--cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
+-Wall -Wextra -Wpedantic
